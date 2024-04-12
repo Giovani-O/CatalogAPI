d6fcbddb0a866868e5d03c2cecebf157aebe19d7
Giovani de Oliveira <giovani98@proton.me>
2024-04-12 15:31:30 -0300

    feat: Run migrations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,6 +1,9 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogAPI.Models;
+[Table("Categories")]
 public class Category
 {
   public Category()
@@ -8,8 +11,15 @@ public class Category
     Products = new Collection<Product>(); // Initialize the products collection in the constructor
   }
 
+  [Key]
   public int Id { get; set; }
+
+  [Required]
+  [StringLength(80)]
   public string? Name { get; set; }
+
+  [Required]
+  [StringLength(300)]
   public string? ImageUrl { get; set; }
 
   public ICollection<Product>? Products { get; set; } // Navigation property, Category will contain a collection of products
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,13 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CatalogAPI.Models;
+[Table("Products")]
 public class Product
 {
+  [Key]
   public int Id { get; set; }
+
+  [Required]
+  [StringLength(80)]
   public string? Name { get; set; }
+
+  [Required]
+  [StringLength(300)]
   public string? Description { get; set; }
+
+  [Required]
+  [Column(TypeName = "decimal(10,2)")]
   public decimal Price { get; set; }
+
+  [Required]
+  [StringLength(300)]
   public string? ImageUrl { get; set; }
+
   public float Stored { get; set; }
+
   public DateTime RegisterDate { get; set; }
+
   public int CategoryId { get; set; } // Defines foreign key
+
   public Category? Category { get; set; } // Navigation property, Product will have a category
 }
