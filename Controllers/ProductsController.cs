3cc1f7d69c22ede8728596eabb27bb0e5054d83a
Giovani de Oliveira <giovani98@proton.me>
2024-04-15 14:17:36 -0300

    feat: Create CRUD for products

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using CatalogAPI.Context;
 using CatalogAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("[controller]")]
@@ -16,8 +17,59 @@ public class ProductsController : ControllerBase
   [HttpGet]
   public ActionResult<IEnumerable<Product>> Get()
   {
-    var products = _context?.Products?.ToList();
-    if (products is null) return NotFound();
+    var products = _context.Products?.ToList();
+    if (products is null) return NotFound(
+      "Não é possível encontrar produtos. Tente novamente mais tarde."
+    );
     return products;
+  }
+
+  [HttpGet("{id:int}", Name = "GetProduct")]
+  public ActionResult<Product> Get(int id)
+  {
+    var product = _context.Products?.FirstOrDefault(x => x.Id == id);
+    if (product is null) return NotFound("Produto não encontrado");
+    return product;
+  }
+
+  [HttpPost]
+  public ActionResult Post(Product product)
+  {
+    if (product is null) return BadRequest("");
+
+    _context?.Products?.Add(product);
+    _context?.SaveChanges();
+
+    // Returns the newly created product and the HTTP Code 201
+    return new CreatedAtRouteResult(
+      "GetProduct", new { id = product.Id }, product
+    );
+  }
+
+  [HttpPut("{id:int}")]
+  public ActionResult Put(int id, Product product)
+  {
+    if (id != product.Id) return BadRequest("");
+
+    // Marks the entity as modified and save the changes
+    _context.Entry(product).State = EntityState.Modified;
+    _context.SaveChanges();
+
+    return Ok(product);
+  }
+
+  [HttpDelete("{id:int}")]
+  public ActionResult Delete(int id)
+  {
+    if (id <= 0) return BadRequest("");
+
+    var product = _context.Products?.FirstOrDefault(x => x.Id == id);
+
+    if (product is null) return NotFound("Produto não encontrado");
+
+    _context.Products?.Remove(product);
+    _context.SaveChanges();
+
+    return Ok();
   }
 }
