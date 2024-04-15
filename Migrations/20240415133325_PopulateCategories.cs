78d9573f24146b26bc046b84bcb7422993ee8d02
Giovani de Oliveira <giovani98@proton.me>
2024-04-15 11:11:36 -0300

    feat: Create migrations

---
+++ Migrations/20240415133325_PopulateCategories.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateCategories : Migration
+    {
+        /// <inheritdoc />
+        /// Up() is what will be executed when we run the migration
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO categories(Name, ImageUrl) VALUES('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO categories(Name, ImageUrl) VALUES('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO categories(Name, ImageUrl) VALUES('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        /// Down() is what will run when we remove the migration
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from categories");
+        }
+    }
+}
---
+++ Migrations/20240415140452_PopulateProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO products(Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+            "VALUES('Coca-Cola', 'Latinha 350ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+
+            mb.Sql("INSERT INTO products(Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+            "VALUES('Australian Burger', 'Pão australiano, hamburguer artesanal 90g, rúcula, tomate em rodelas, onion rings, bacon e queijo cheddar', 37.50, 'hamburguer.jpg', 999, now(), 2)");
+
+            mb.Sql("INSERT INTO products(Name, Description, Price, ImageUrl, Stock, RegisterDate, CategoryId) " +
+            "VALUES('Açaí', 'Copo 400ml com complementos personalizados', 14.99, 'acai.jpg', 999, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM products");
+        }
+    }
+}
