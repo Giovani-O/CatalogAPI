f9ff4928760c3a4965ba71032eee7391fa43f9ba
Giovani de Oliveira <giovani98@proton.me>
2024-05-03 09:47:17 -0300

    feat: Create token generation service

---
+++ Services/TokenService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CatalogAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        public JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims, IConfiguration _config)
+        {
+            // Get secret key
+            var key = _config.GetSection("JWT").GetValue<string>("SecretKey") ?? 
+                throw new InvalidOperationException("Invalid secret key!");
+
+            // Convert secret key to an array of bytes
+            var privateKey = Encoding.UTF8.GetBytes(key);
+
+            // Creating signature credentials
+            var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(privateKey),
+                SecurityAlgorithms.HmacSha256Signature);
+
+            // Token descriptor
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_config.GetSection("JWT")
+                    .GetValue<double>("TokenValidityInMinutes")),
+                Audience = _config.GetSection("JWT").GetValue<string>("ValidAudience"),
+                Issuer = _config.GetSection("JWR").GetValue<string>("ValidIssuer"),
+                SigningCredentials = signingCredentials
+            };
+
+            // Generates the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateJwtSecurityToken(tokenDescriptor);
+
+            return token;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            // Create a new array of bytes
+            var secureRandomBytes = new byte[128];
+
+            // Random numbers
+            using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+            // Fill secureRandomBytes with random bytes
+            randomNumberGenerator.GetBytes(secureRandomBytes);
+
+            // Converting the bytes to base64
+            var refreshToken = Convert.ToBase64String(secureRandomBytes);
+            return refreshToken;
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
+        {
+            // get secret key
+            var secretKey = _config["JWT:SecretKey"] ?? throw new InvalidOperationException("Invalid key");
+
+            // Set validation parameters
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(secretKey)),
+                ValidateLifetime = false,
+            };
+
+            // Validate the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(
+                token, 
+                tokenValidationParameters, 
+                out SecurityToken securityToken
+            );
+
+            // Check if the security token is a JwtSecurityToken
+            if (securityToken is not JwtSecurityToken jwtSecurityToken ||
+                !jwtSecurityToken.Header.Alg
+                .Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase)
+            )
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            return principal;
+        }
+    }
+}
